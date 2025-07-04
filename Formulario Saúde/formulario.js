cca6344cd0c07c3856dac8cf8ba6ad3b7d96850a
Caio Vinicius <caioviniaires@gmail.com>
2025-07-03 22:12:26 -0300

    atvidades terminadas

---
+++ Formulario Saúde/formulario.js
@@ -0,0 +1,88 @@
+// Executar mascaras
+function mascara(o, f) //Define o objeto e chama a função
+{
+    objeto = o
+    funcao = f
+    setTimeout("executaMascara()", 1)
+
+}
+
+function executaMascara() {
+    objeto.value = funcao(objeto.value)
+
+}
+
+//Telefone
+function telefone(variavel) {
+    variavel = variavel.replace(/\D/g, "")
+    variavel = variavel.replace(/^(\d\d)(\d)/g, "($1)$2")//ADICIONA PARENTESES EM VOLTA DOS DOIS PRIMEIROS DIGITOS
+
+    variavel = variavel.replace(/(\d{5})(\d)/g, "$1-$2")//ADICIONA HIFEN ENTRE O QUARTO E QUINTO DIGITO
+
+    return variavel
+}
+
+//rg e cpf
+function RGeCPF(variavel) 
+{
+    variavel = variavel.replace(/\D/g, "")
+
+    variavel = variavel.replace(/(\d{3})(\d)/, "$1.$2") //ADICIONA O PONTO 
+
+    variavel = variavel.replace(/(\d{3})(\d)/, "$1.$2") //ADICIONA O PONTO 
+
+    variavel = variavel.replace(/(\d{3})(\d{1,2})$/, "$1-$2") //ADICIONA O HIFEN 
+
+    return variavel
+}
+
+//Cep
+function cep(variavel) 
+{
+    variavel = variavel.replace(/\D/g, "")
+
+    variavel = variavel.replace(/(\d{2})(\d)/, "$1.$2") //ADICIONA O PONTO 
+
+
+    variavel = variavel.replace(/(\d{3})(\d{1,2})$/, "$1-$2") //ADICIONA O HIFEN 
+
+    return variavel
+}
+
+//cartão do sus
+function cartaosus(variavel) 
+{
+    variavel = variavel.replace(/\D/g, "")
+
+    variavel = variavel.replace(/(\d{6})(\d)/, "$1-$2") //ADICIONA O HIFEN
+
+
+    return variavel
+}
+
+//data
+function dtnascimento(variavel) 
+{
+    variavel = variavel.replace(/\D/g, "")
+
+    variavel = variavel.replace(/(\d{2})(\d)/, "$1/$2") //ADICIONA A BARRA
+    
+    variavel = variavel.replace(/(\d{2})(\d)/, "$1/$2") //ADICIONA A BARRA
+
+
+    return variavel
+}
+
+//data
+function data(variavel) 
+{
+    variavel = variavel.replace(/\D/g, "")
+
+    variavel = variavel.replace(/(\d{2})(\d)/, "$1/$2") //ADICIONA A BARRA
+
+    variavel = variavel.replace(/(\d{2})(\d)/, "$1/$2") //ADICIONA A BARRA
+
+
+    return variavel
+}
+
